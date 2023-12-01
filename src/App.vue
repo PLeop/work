3ccc0c6a0198dd5caf3511b83b9d49fa70ba30b0
Paul Leopold <paul.elias.leopold@gmail.com>
2023-12-01 15:10:06 +0100

    erste Preiskalkulation implementiert

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MainView msg="PlaceHolder - Titel oder sowas"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainView from './components/Main.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainView
   }
 }
 </script>
